6388e692d99e9e5fd779d7f43500ec469bdc916b
Bustah <psnatb2lme@gmail.com>
2025-01-30 11:29:40 +0800

    orbit controls

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
+//Get Canvas Dom
+const canvas = document.getElementById('canvas')
 
 //Create a new Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('#fffffff')
+scene.background = new THREE.Color('#FFFFFF')
 //Create and setup Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
 camera.position.z = 5;
@@ -12,10 +15,12 @@ const light = new THREE.DirectionalLight(0xffffff, 10);
 light.position.set(1,1,1);
 scene.add(light);
 //Create and setup The Renderer
-const render = new THREE.WebGLRenderer();
+const render = new THREE.WebGLRenderer({canvas});
 render.setSize(window.innerWidth, window.innerHeight);
+render.setPixelRatio(window.devicePixelRatio);
+
 //append renderer to body
-document.body.appendChild(render.domElement);
+// document.body.appendChild(render.domElement);
 
 //Create a 3D shape
 const Geometry = new THREE.BoxGeometry();
@@ -26,6 +31,13 @@ const cube = new THREE.Mesh(Geometry, material);
 //add shape to scene
 scene.add(cube);
 
+//Orbit Controls
+const control = new OrbitControls(camera, render.domElement);
+control.enableDamping = true;
+control.dampingFactor = 0.05;
+control.enableZoom = true;
+control.enablePan = true;
+
 
 //Makes the shape rotate
 function animate(){
@@ -33,6 +45,9 @@ function animate(){
 
     cube.rotation.x +=0.01;
     cube.rotation.y +=0.01;
+
+    control.update();
+
     render.render(scene, camera);
 }
 
